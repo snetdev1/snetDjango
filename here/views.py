e778a92fa5c1e2e7f450271dae32858cf9c31450
Paul <s.net.dev1@gmail.com>
2016-07-31 18:06:41 -0400

    permissions update
    corrected permissions for parties

--- here/views.py
+++ here/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from serializers import PartySerializer
 from models import *
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 
 # Create your views here.
 class PartyViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows projects to be viewed or edited.
     """
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
     queryset = party.objects.all()
     serializer_class = PartySerializer
